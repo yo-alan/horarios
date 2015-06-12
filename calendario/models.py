d7ff5a952ae8b41bdcf049f9d113c9e6235ea752
yo-alan <marchanalan@hotmail.com>
2015-06-12 20:59:16 -0300

    Solucionado el problema de la generacion inicial. Y una manera mas vistosa de mostrar un calendario.

--- calendario/models.py
+++ calendario/models.py
@@ -1,5 +1,11 @@
+import random
+
 from django.db import models
 
+
+dias = {0 : "Domingo", 1 : "Lunes", 2 : "Martes", 3 : "Miercoles", 4 : "Jueves", 5 : "Viernes", 6 : "Sabado"}
+#HARDCODED
+horarios = {1 : "7:30", 2 : "8:10", 3 : "9:00", 4 : "9:40", 5 : "10:30", 6 : "11:10", 7 : "11:50"}
 
 class Especialidad(models.Model):
 	
@@ -30,20 +36,33 @@ class Profesional(models.Model):
 class Calendario(models.Model):
 	
 	nombre = models.CharField(max_length=100, blank=True)
-	horarios = [[], [], [], [], [], [], []]
+	horarios = []
 	puntaje = models.IntegerField(default=0)
 	ranking_distribucion = models.FloatField(default=100)
 	
 	def getHorarios(self, ):
 		
-		hs = Horario.objects.filter(calendario=self).order_by('hora_desde')
+		hs = Horario.objects.filter(calendario=self).order_by('hora_desde', 'dia_semana')
 		
-		self.horarios = [[], [], [], [], [], [], []]
+		self.horarios = []
 		
 		for h in hs:
-			self.horarios[h.dia_semana].append(h)
+			self.agregar_horario(h)
 		
 		return self.horarios
+	
+	def agregar_horario(self, horario):
+		
+		if self.horarios == []:
+			self.horarios.append([horario])
+			return
+		
+		for hs in self.horarios:
+			if hs[0].hora_desde == horario.hora_desde:
+				hs.append(horario)
+				return
+		
+		self.horarios.append([horario])
 	
 	def cruce(self, madre):
 		pass
@@ -58,10 +77,11 @@ class Horario(models.Model):
 	calendario = models.ForeignKey(Calendario, null=False)
 	
 	def __str__(self, ):
-		return str(self.profesional.especialidad)
+		#~ return str(self.profesional.especialidad)
+		return str(self.hora_desde)
 	
 	def __eq__(self, o):
-		return self.hora_desde == o.hora_desde and self.hora_hasta == o.hora_hasta and self.dia_semana == o.dia_semana
+		return self.hora_desde == o.hora_desde and self.hora_hasta == o.hora_hasta and self.dia_semana == o.dia_semana and self.calendario == o.calendario
 	
 
 class Restriccion(models.Model):
@@ -69,7 +89,7 @@ class Restriccion(models.Model):
 	hora_desde = models.TimeField('desde', blank=True)
 	hora_hasta = models.TimeField('hasta', blank=True)
 	dia_semana = models.IntegerField(default=0, blank=True)
-	#FALTA TERMINAR DE DEFINIRLO
+	#FALTA TERMINAR DE DEFINIRLO UNA RESTRICCION GENERICAS, PARA ESPACIO Y PROF
 	
 	def __eq__(self, o):
 		pass
@@ -86,12 +106,60 @@ class Ecosistema(object):
 	restricciones = []
 	generaciones = 0
 	
-	def __init__(self, rs, generaciones=50):
-		self.restricciones = rs
+	def __init__(self, generaciones=50):
+		self.restricciones = Restriccion.objects.all()
 		self.generaciones = generaciones
 	
 	def generar_poblacion_inicial(self, ):
-		pass
+		
+		ps = Profesional.objects.all()
+		
+		#DETERMINAMOS TODAS LAS POSIBLES COMBINACIONES DE HORARIOS
+		#HARDCODED
+		for i in range(1, 6):
+			for j in range(1, len(horarios)):
+				for k in ps:
+					c = Calendario()
+					c.save()
+					h = Horario()
+					h.profesional = Profesional.objects.get(pk=k.id)
+					h.hora_desde = horarios[j]
+					h.hora_hasta = horarios[j+1]
+					h.dia_semana = i
+					h.calendario = c
+					h.save()
+					
+					c.agregar_horario(h)
+					
+					self.poblacion.append(c)
+					
+		
+		#LLENAMOS LOS CALENDARIOS CON LOS HORARIOS FALTANTES
+		for c in self.poblacion:
+			
+			#HARDCODED
+			for dia in range(1, 6):
+				
+				for horario in range(1, len(horarios)):
+					
+					h = Horario(profesional=ps[random.randrange(1, len(ps))], hora_desde=horarios[horario], hora_hasta=horarios[horario+1], dia_semana=dia, calendario=c)
+					
+					existe = False
+					for dh in c.horarios:
+						for hh in dh:
+							if h == hh:
+								print str(h.hora_desde) + " == " + str(hh.hora_desde)
+								print str(h.hora_hasta) + " == " + str(hh.hora_hasta)
+								print str(h.dia_semana) + " == " + str(hh.dia_semana)
+								existe = True
+					
+					if existe:
+						continue
+					
+					h.save()
+					
+					c.agregar_horario(h)
+		
 	
 	def evolucionar(self, ):
 		pass
@@ -103,7 +171,42 @@ class Ecosistema(object):
 		
 		aptitud_val = 0
 		
-		#TO DO
+		#PREGUNTAR SI SE EVALUAN INDIVIDUALMENTE O TODA LA POBLACION
+		rs = Restriccion.objects.all()
+		
+		for c in cs:
+			
+			for h in c:
+				
+				for r in rs:
+					
+					if (h.desde >= r.desde and h.desde <= r.hasta) or \
+						(h.hasta >= r.desde and h.hasta <= r.hasta) or \
+						(h.desde <= r.desde and h.hasta >= r.hasta):#OJOOOOOOO h.desde <= r.desde???
+						c.puntaje = c.puntaje + 1
+						
+		
+		es = Especialidad.objects.all()
+		
+		for c in cs:
+			
+			for e in es: #e = especializacion , es = especializaciones
+				
+				horas_semanales = 0
+				for i in range(0, 6):
+					
+					for h in c.horarios[i]:
+						
+						if h.profesional.especialidad == e:
+							horas_semanales = horas_semanales + 1
+					
+					if horas_semanales != e.carga_horaria_semanal:
+						#-20% (del ranking)
+						x = abs(e.carga_semanal - contador)
+						#HARDCODED
+						y = (35 - e.carga_semanal) / (x * 100)
+						#HARDCODED
+						p_total = p_total + (100 / len(especialidad) / (20 + y) * 100)
 		
 		return aptitud_val
 	
--- calendario/views.py
+++ calendario/views.py
@@ -1,53 +1,22 @@
-import random, time
+import time
 
 from django.shortcuts import render, get_object_or_404
 
-from .models import Calendario, Profesional, Horario, Restriccion, Especialidad
+from .models import Ecosistema, Calendario, Profesional, Horario, Restriccion, Especialidad
 
-
-dias = {0 : "Domingo", 1 : "Lunes", 2 : "Martes", 3 : "Miercoles", 4 : "Jueves", 5 : "Viernes", 6 : "Sabado"}
-#HARDCODED
-horarios = {1 : "7:30", 2 : "8:10", 3 : "9:00", 4 : "9:40", 5 : "10:30", 6 : "11:10", 7 : "11:50"}
-
-#RESQUESTS
 
 def index(request):
 	
 	start_time = time.time()
 	
-	ps = Profesional.objects.all()
+	e = Ecosistema()
 	
-	hs = generar_horarios(ps)
-	
-	cs = [] #La poblacion de calendarios
-	
-	for h in hs:
-		
-		c = Calendario()
-		c.save()
-		c.horarios.append(h)
-		h.calendario = c
-		h.save()
-		cs.append(c)
-	
-	for c in cs:
-		
-		#HARDCODED
-		for dia in range(1, 6):
-			
-			for horario in range(1, len(horarios)):
-				
-				h = Horario(profesional=ps[random.randrange(1, len(ps))], hora_desde=horarios[horario], hora_hasta=horarios[horario+1], dia_semana=dia, calendario=c)
-				
-				#if h not in c.horarios[h.dia_semana]:
-					
-				h.save()
-				
-				c.horarios[h.dia_semana].append(h)
+	e.generar_poblacion_inicial()
 	
 	
 	tiempo = (time.time() - start_time)
-	context = {'cs': cs, 'tiempo' : tiempo, 'dias' : range(5), 'horas' : range(6)}
+	
+	context = {'cs': e.poblacion, 'tiempo' : tiempo}
 	
 	return render(request, 'calendario/index.html', context)
 
@@ -56,66 +25,4 @@ def detail(request, calendario_id):
 	
 	calendario = get_object_or_404(Calendario, pk=calendario_id)
 	
-	return render(request, 'calendario/detail.html', {'calendario': calendario})
-
-
-#FIN RESQUESTS
-
-def generar_horarios(ps):
-
-	hs = []
-
-	l = 1
-	for i in range(1, 6):
-		for j in range(1, len(horarios)):
-			for k in ps:
-				h = Horario()
-				h.profesional = Profesional.objects.get(pk=k.id)
-				h.hora_desde = horarios[j]
-				h.hora_hasta = horarios[j+1]
-				h.dia_semana = i
-				h.save()
-				
-				hs.append(h)
-				
-				l = l + 1
-
-	return hs
-
-def aptitud(cs):
-	
-	rs = Restriccion.objects.all()
-	
-	for c in cs:
-		
-		for h in c:
-			
-			for r in rs:
-				
-				if (h.desde >= r.desde and h.desde <= r.hasta) or \
-					(h.hasta >= r.desde and h.hasta <= r.hasta) or \
-					(h.desde <= r.desde and h.hasta >= r.hasta):#OJOOOOOOO h.desde <= r.desde???
-					c.puntaje = c.puntaje + 1
-					
-	
-	es = Especialidad.objects.all()
-	
-	for c in cs:
-		
-		for e in es: #e = especializacion , es = especializaciones
-			
-			horas_semanales = 0
-			for i in range(0, 6):
-				
-				for h in c.horarios[i]:
-					
-					if h.profesional.especialidad == e:
-						horas_semanales = horas_semanales + 1
-				
-				if horas_semanales != e.carga_horaria_semanal:
-					#-20% (del ranking)
-					x = abs(e.carga_semanal - contador)
-					#HARDCODED
-					y = (35 - e.carga_semanal) / (x * 100)
-					#HARDCODED
-					p_total = p_total + (100 / len(especialidad) / (20 + y) * 100)
+	return render(request, 'calendario/detail.html', {'calendario': calendario, 'dias' : range(5), 'horas' : range(6)})
