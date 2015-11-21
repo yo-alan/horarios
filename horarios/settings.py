cf25e11a33fb92a17c0302ebddb52ea5580bb4b1
yo-alan <marchanalan@hotmail.com>
2015-11-21 02:42:29 -0300

    Settings actualizado.

--- horarios/settings.py
+++ horarios/settings.py
@@ -104,6 +104,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join("/", 'static')
 
 STATIC_URL = '/static/'
