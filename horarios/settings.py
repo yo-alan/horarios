e7d070e239d0f07d2048a00c34f885cc404b356d
yo-alan <marchanalan@hotmail.com>
2015-11-21 03:03:28 -0300

    Settings actualizado.

--- horarios/settings.py
+++ horarios/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '20++qs0053gm&it3&c0dpsbi=(*i)9-z1s7pmgbyt@#dvozfk9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
