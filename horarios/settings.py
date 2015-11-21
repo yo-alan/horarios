819f10fb5cf8e266437f5d894e9779dca61ce1cf
yo-alan <marchanalan@hotmail.com>
2015-11-21 13:45:12 -0300

    Settings actualizado.

--- horarios/settings.py
+++ horarios/settings.py
@@ -119,7 +119,7 @@ ALLOWED_HOSTS = ['*']
 
 #~ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = '/app/horariosudc/calendario'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
