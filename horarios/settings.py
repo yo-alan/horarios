1aa9e18e45b8ecafbaec6bf306693e7bfd7c11ef
yo-alan <marchanalan@hotmail.com>
2015-11-21 23:05:10 -0300

    Settings actualizado.

--- horarios/settings.py
+++ horarios/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'horarios.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'horarios',
+        'NAME': 'd8j7teeapeqcir',
         'USER': 'django',
         'PASSWORD': 'django',
         'HOST': '127.0.0.1',
